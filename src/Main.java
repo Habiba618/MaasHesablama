9b78f46f020903b1931dddb2fd454d20da609091
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-18 10:16:33 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+Employee emp = new Employee("Habibe", 95832,5,2023);
+        System.out.println(emp.toString());
     }
 }
