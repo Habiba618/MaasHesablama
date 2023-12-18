f2a4db107d9187797f110e87bd7e076411d58386
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-18 10:16:53 +0400

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.Calendar;
+
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public double tax() {
+        if (salary <= 1000) {
+            return 0;
+        } else {
+            return salary * 0.03;
+        }
+    }
+
+    public double bonus() {
+        if (workHours >= 40) {
+            return 30 * (workHours - 40);
+        } else {
+            return 0;
+        }
+    }
+
+    public double raiseSalary() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int yearsWorked = currentYear - hireYear;
+
+        if (yearsWorked < 10) {
+            return salary * 0.05;
+        } else if (yearsWorked >= 10 && yearsWorked < 20) {
+            return salary * 0.1;
+        } else {
+            return salary * 0.15;
+        }
+    }
+
+    public double totalSalary() {
+        double tax = tax();
+        double bonus = bonus();
+        double raise = raiseSalary();
+        return salary + raise - tax + bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Adı: " + name +
+                "\nMaaşı: " + salary +
+                "\nÇalışma Saati: " + workHours +
+                "\nBaşlangıç Yılı: " + hireYear +
+                "\nVergi: " + tax() +
+                "\nBonus: " + bonus() +
+                "\nMaaş Artışı: " + raiseSalary() +
+                "\nVergi ve Bonuslar ile birlikte maaş: " + totalSalary() +
+                "\nToplam Maaş: " + (salary + raiseSalary());
+    }
 }
